bfd718acc5abb8bee07a6298d356d1624abe550a
hp <you@example.com>
2023-12-10 13:50:06 +0530

    changes in mongo url

--- app.js
+++ app.js
@@ -35,8 +35,6 @@ const PORT = process.env.PORT || 8000
 
 mongoose
 .connect(process.env.MONGO_URL, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
 })
   .then(() => console.log("DB Connection Successfull!"))
   .catch((err) => {
